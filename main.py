fa371c906f40c2ae18d2cbf796b1a4571aaaff84
6jeez <6jeezdev@gmail.com>
2024-05-28 00:14:22 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from config import TOKEN
+from handlers import user_commands, btn1
+
+
+async def main():
+    bot = Bot(TOKEN)
+    dp = Dispatcher()
+
+    dp.include_routers(user_commands.router, btn1.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
